095f6802390d67995cb47859b309a0a8caed4ca4
Julian Perdomo <jperdomo.nerd@gmail.com>
2025-12-17 18:37:41 -0500

    Debug 5 with more resources

--- Server.js
+++ Server.js
@@ -1,46 +1,31 @@
 const express = require('express');
-const { chromium } = require('playwright');
+const puppeteer = require('puppeteer')
 
 const app = express();
 
 async function getTend() {
-  const browser = await chromium.launch({ headless: false });
-  const context = await browser.newContext();
-  const page    = await context.newPage();
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
 
-  await page.goto('https://suameca.banrep.gov.co/estadisticas-economicas-back/reporte-oac.html?path=%2FEstadisticas_Banco_de_la_Republica%2F4_Sector_Externo_tasas_de_cambio_y_derivados%2F1_Tasas_de_cambio%2F1_Tasa_de_cambio_del_peso_colombiano_por_USD(TRM)%2F2_Mercado_Interbancario_de_Divisas%2F1_Mercado_interbancario_de_divisas',
-    {
-      waitUntil: 'networkidle',
-      timeout: 120000
-    }
-  );
+  const page = await browser.newPage();
 
-  // const browser = await chromium.launch({
-  //   headless: true,
-  //   args: ['--no-sandbox']
-  // });
-
-  // const context = await browser.newContext();
-  // const page = await context.newPage();
-
-  // try {
-  //   await page.goto(
-  //     'https://suameca.banrep.gov.co/estadisticas-economicas-back/reporte-oac.html?path=%2FEstadisticas_Banco_de_la_Republica%2F4_Sector_Externo_tasas_de_cambio_y_derivados%2F1_Tasas_de_cambio%2F1_Tasa_de_cambio_del_peso_colombiano_por_USD(TRM)%2F2_Mercado_Interbancario_de_Divisas%2F1_Mercado_interbancario_de_divisas',
-  //     {
-  //       waitUntil: 'networkidle',
-  //       timeout: 120000
-  //     }
-  //   );
+  await page.goto(
+      'https://suameca.banrep.gov.co/estadisticas-economicas-back/reporte-oac.html?path=%2FEstadisticas_Banco_de_la_Republica%2F4_Sector_Externo_tasas_de_cambio_y_derivados%2F1_Tasas_de_cambio%2F1_Tasa_de_cambio_del_peso_colombiano_por_USD(TRM)%2F2_Mercado_Interbancario_de_Divisas%2F1_Mercado_interbancario_de_divisas',
+      {
+        waitUntil: 'networkidle0',
+        timeout: 120000
+      }
+    );
 
     const values = await page.$$eval(
       '.bi_viz_gridview_cell_text_nowrap',
       els => els.map(e => e.innerText.trim())
     );
 
-    const todayExchangeRate = parseFloat(values[5].replace(',', ''));
+    const todayExchangeRate   = parseFloat(values[5].replace(',', ''));
     const closingExchangeRate = parseFloat(values[13].replace(',', ''));
-
-    await browser.close();
 
     return {
       todayExchangeRate,
