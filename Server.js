ea44c0743948511fc49f7bd29c1b7a770e583f9f
Julian Perdomo <jperdomo.nerd@gmail.com>
2025-12-16 23:40:40 -0500

    Add getTend function

--- Server.js
+++ Server.js
@@ -1,10 +1,45 @@
 const express = require('express')
+const puppeteer = require('puppeteer')
 
 const app = express()
 
+async function getTend() {
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+
+  try {
+    const page = await browser.newPage();
+
+    await page.goto(
+      'https://suameca.banrep.gov.co/estadisticas-economicas-back/reporte-oac.html?path=%2FEstadisticas_Banco_de_la_Republica%2F4_Sector_Externo_tasas_de_cambio_y_derivados%2F1_Tasas_de_cambio%2F1_Tasa_de_cambio_del_peso_colombiano_por_USD(TRM)%2F2_Mercado_Interbancario_de_Divisas%2F1_Mercado_interbancario_de_divisas',
+      {
+        waitUntil: 'networkidle0',
+        timeout: 120000
+      }
+    );
+
+    const values = await page.$$eval(
+      '.bi_viz_gridview_cell_text_nowrap',
+      els => els.map(e => e.innerText.trim())
+    );
+
+    const todayExchangeRate = parseFloat(values[4].replace(',', '.'));
+    const closingExchangeRate = parseFloat(values[13].replace(',', '.'));
+
+    return {
+      todayExchangeRate,
+      closingExchangeRate
+    };
+  } finally {
+    await browser.close();
+  }
+}
+
 //routing path
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(getTend());
 });
 
 // Start the server
