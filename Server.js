81e56211adb203a31ec6e593fd11d8fa9308780a
Julio Ruiz <juliocruizt@gmail.com>
2025-12-17 20:36:19 -0500

    Se cambia commonjs por module

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const puppeteer = require('puppeteer')
+import puppeteer from 'puppeteer';
+import express from 'express';
+
 
 const app = express();
 
@@ -15,25 +16,25 @@ async function getTend() {
   const page = await browser.newPage();
 
   await page.goto(
-      'https://suameca.banrep.gov.co/estadisticas-economicas-back/reporte-oac.html?path=%2FEstadisticas_Banco_de_la_Republica%2F4_Sector_Externo_tasas_de_cambio_y_derivados%2F1_Tasas_de_cambio%2F1_Tasa_de_cambio_del_peso_colombiano_por_USD(TRM)%2F2_Mercado_Interbancario_de_Divisas%2F1_Mercado_interbancario_de_divisas',
-      {
-        waitUntil: 'networkidle0',
-        timeout: 120000
-      }
-    );
+    'https://suameca.banrep.gov.co/estadisticas-economicas-back/reporte-oac.html?path=%2FEstadisticas_Banco_de_la_Republica%2F4_Sector_Externo_tasas_de_cambio_y_derivados%2F1_Tasas_de_cambio%2F1_Tasa_de_cambio_del_peso_colombiano_por_USD(TRM)%2F2_Mercado_Interbancario_de_Divisas%2F1_Mercado_interbancario_de_divisas',
+    {
+      waitUntil: 'networkidle0',
+      timeout: 120000
+    }
+  );
 
-    const values = await page.$$eval(
-      '.bi_viz_gridview_cell_text_nowrap',
-      els => els.map(e => e.innerText.trim())
-    );
+  const values = await page.$$eval(
+    '.bi_viz_gridview_cell_text_nowrap',
+    els => els.map(e => e.innerText.trim())
+  );
 
-    const todayExchangeRate   = parseFloat(values[5].replace(',', ''));
-    const closingExchangeRate = parseFloat(values[13].replace(',', ''));
+  const todayExchangeRate = parseFloat(values[5].replace(',', ''));
+  const closingExchangeRate = parseFloat(values[13].replace(',', ''));
 
-    return {
-      todayExchangeRate,
-      closingExchangeRate
-    };
+  return {
+    todayExchangeRate,
+    closingExchangeRate
+  };
   // } finally {
   //   await browser.close();
   // }
