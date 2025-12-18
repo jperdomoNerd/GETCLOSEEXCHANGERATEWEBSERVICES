f3d888f07d2071d6d491fe96ac5ca2fe41310a3a
Julio Ruiz <juliocruizt@gmail.com>
2025-12-17 21:55:10 -0500

    Se corrige puerto en  el server

--- Server.js
+++ Server.js
@@ -66,6 +66,8 @@ app.get('/', async (req, res) => {
 });
 
 // start server
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log('Server started on port 3000');
 });
